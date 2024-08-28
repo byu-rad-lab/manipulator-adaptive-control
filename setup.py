fafd853781cec4430cc243aaed1ed14289c7c0ec
Curtis <curtiscarljohnson@gmail.com>
2024-08-28 13:57:40 -0600

    updating package deps.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='manipulator-adaptive-control',
+    version='0.1',
+    author='Curtis C. Johnson',
+    description='Adaptive control for robot manipulators',
+    author_email='curtiscarljohnson@gmail.com',
+    license='MIT',
+    url='https://github.com/byu-rad-lab/manipulator-adaptive-control',
+    packages=find_packages(),
+)
